347e22834517f00e103c3dbb5f8d271a97ea5289
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-07-07 19:23:20 -0300

    feat: :sparkles: Themes

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primary = Color(0xFF6200EE);
+  static final secondary = Color(0xFF03DAC5);
+  static final red = Color(0xFFFF0000);
+  static final white = Color(0xFFFFFFFF);
+  static final black = Color(0xFF000000);
+}
---
+++ lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:arrivederci/shared/themes/app_colors.dart';
+
+class TextStyles {
+  static final splashTitle = GoogleFonts.rougeScript(
+    fontSize: 72,
+    fontWeight: FontWeight.w400,
+    color: AppColors.white,
+  );
+
+  static final pageTitle = GoogleFonts.roboto(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: AppColors.black,
+  );
+
+  static final filledButton = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.white,
+  );
+
+  static final unfilledButton = GoogleFonts.roboto(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: AppColors.primary,
+  );
+
+  static final textBlack = GoogleFonts.roboto(
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+    color: AppColors.primary,
+  );
+}
