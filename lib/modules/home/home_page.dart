0af554ef6a32f75a6dcda2516274c7f2ebae74d6
Alan Pinho <alanqupinho@gmail.com>
2021-07-17 15:28:06 -0300

    feat: :sparkles: Home BottonAppBar

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:arrivederci/shared/themes/app_colors.dart';
+import 'package:arrivederci/shared/themes/app_text_styles.dart';
 import 'package:arrivederci/shared/widgets/navigation_drawer/navigation_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +18,22 @@ class _HomeState extends State<Home> {
         title: Text("Arrivederci"),
       ),
       drawer: NavigationDrawer(),
+
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: AppColors.primary,
+          height: 50.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Sobre - Arrivederci © 2021",
+                style: TextStyles.textWhite
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
