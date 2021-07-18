428b387bf66c892773f74ccee9473726ee938eb5
Alan Pinho <alanqupinho@gmail.com>
2021-07-18 15:22:36 -0300

    feat: :sparkles: About page

--- lib/modules/about/about_page.dart
+++ lib/modules/about/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:arrivederci/shared/themes/app_footer.dart';
+import 'package:arrivederci/shared/themes/app_text_styles.dart';
 import 'package:arrivederci/shared/widgets/navigation_drawer/navigation_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
@@ -11,8 +12,34 @@ class About extends StatelessWidget {
       appBar: AppBar(
         title: Text("Sobre"),
       ),
-      drawer: NavigationDrawer(),
-      
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
+            child:Text(
+            "Trabalho da disciplina de Programação para Dispositivos Móveis",
+            style: TextStyles.textAbout,
+            textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
+            child:Text(
+            "Alunos:",
+            style: TextStyles.textAbout,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
+            child:Text(
+            "Alan Queiroz Pinho \n"+
+            "Ângelo T. Bittencourt Todeschini\n"+
+            "João Vitor de Moura Rosa",
+            style: TextStyles.textAbout,
+            ),
+          )
+        ],
+      ),
       bottomNavigationBar: AppFooter.footer,
     );
   }
