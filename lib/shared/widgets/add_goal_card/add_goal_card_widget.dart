1dbf8614129ee1fffb70d1bac8479fd43ef0f57c
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-07-18 15:53:08 -0300

    fix: :bug: Place not found exception

--- lib/shared/widgets/add_goal_card/add_goal_card_widget.dart
+++ lib/shared/widgets/add_goal_card/add_goal_card_widget.dart
@@ -2,7 +2,6 @@ import 'package:arrivederci/shared/Constants.dart';
 import 'package:arrivederci/shared/models/place_model.dart';
 import 'package:arrivederci/shared/themes/app_colors.dart';
 import 'package:arrivederci/shared/themes/app_text_styles.dart';
-import 'package:arrivederci/shared/widgets/navigation_drawer/navigation_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
 class AddGoalCard extends StatelessWidget {
@@ -16,58 +15,43 @@ class AddGoalCard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Arrivederci"),
-        leading: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-            color: AppColors.white,
-          ),
-          onPressed: () => Navigator.of(context).pop(),
-        ),
       ),
-      drawer: NavigationDrawer(),
       body: Center(
-        child: Container(
-          width: size.width * 0.9,
-          height: size.height * 0.7,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey.shade200,
-                blurRadius: 2.0,
-                spreadRadius: 0.0,
-                offset: Offset(2.0, 2.0),
-              )
-            ],
-          ),
-          child: Column(
-            children: [
-              ListTile(
-                title: Text(
-                  this.place.name,
-                  style: TextStyles.textBlack,
+        child: Card(
+          child: Container(
+            width: size.width * 0.9,
+            height: size.height * 0.7,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: size.height * 0.05,
                 ),
-              ),
-              ListTile(
-                subtitle: Text(this.place.address),
-              ),
-              Image.network(
-                'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.place.photo}&key=$apiKey',
-                width: size.width,
-                height: size.height * 0.4,
-              ),
-              Text("Avaliação: ${this.place.rating}"),
-              TextButton(
-                onPressed: () {
-                  print("teste");
-                },
-                child: Text(
-                  "+ ADICIONAR AO ROTEIRO",
-                  style: TextStyle(color: AppColors.primary),
+                ListTile(
+                  title: Text(
+                    this.place.name,
+                    style: TextStyles.textBlack,
+                  ),
                 ),
-              )
-            ],
+                ListTile(
+                  subtitle: Text(this.place.address),
+                ),
+                Image.network(
+                  'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.place.photo}&key=$apiKey',
+                  width: size.width,
+                  height: size.height * 0.4,
+                ),
+                Text("Avaliação: ${this.place.rating}"),
+                TextButton(
+                  onPressed: () {
+                    print("teste");
+                  },
+                  child: Text(
+                    "+ ADICIONAR AO ROTEIRO",
+                    style: TextStyle(color: AppColors.primary),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/shared/widgets/location_search/location_search_widget.dart
+++ lib/shared/widgets/location_search/location_search_widget.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:arrivederci/shared/Constants.dart';
 import 'package:arrivederci/shared/models/place_model.dart';
 import 'package:arrivederci/shared/themes/app_colors.dart';
+import 'package:arrivederci/shared/themes/app_text_styles.dart';
 import 'package:arrivederci/shared/widgets/add_goal_card/add_goal_card_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -19,19 +20,50 @@ class _LocationSearchState extends State<LocationSearch> {
 
   final apiKey = GOOGLE_API_KEY;
 
-  Future<Place> searchLocalization() async {
+  Future searchLocalization() async {
     final response = await http.get(Uri.parse(
         "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${searchTextController.text}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours&key=$apiKey"));
 
-    final name = jsonDecode(response.body)['candidates'][0]['name'];
-    final photo = jsonDecode(response.body)['candidates'][0]['photos'][0]
-        ['photo_reference'];
-    final rating =
-        jsonDecode(response.body)['candidates'][0]['rating'].toString();
-    final address =
-        jsonDecode(response.body)['candidates'][0]['formatted_address'];
+    if (jsonDecode(response.body)['status'] != "ZERO_RESULTS") {
+      final name = jsonDecode(response.body)['candidates'][0]['name'];
+      final photo = jsonDecode(response.body)['candidates'][0]['photos'][0]
+          ['photo_reference'];
+      final rating =
+          jsonDecode(response.body)['candidates'][0]['rating'].toString();
+      final address =
+          jsonDecode(response.body)['candidates'][0]['formatted_address'];
 
-    return Place(name: name, address: address, rating: rating, photo: photo);
+      return Place(name: name, address: address, rating: rating, photo: photo);
+    } else {
+      return showDialog(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Lugar não encontrado'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  Text(
+                      'O lugar que você pesquisou não foi encontrado na busca do google maps'),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                child: Text(
+                  'Tudo bem!',
+                  style: TextStyle(color: AppColors.primary),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
@@ -68,12 +100,14 @@ class _LocationSearchState extends State<LocationSearch> {
                       ),
                       onPressed: () async {
                         final place = await searchLocalization();
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => AddGoalCard(place: place),
-                          ),
-                        );
+                        if (place != null) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => AddGoalCard(place: place),
+                            ),
+                          );
+                        }
                       },
                       child: Text('Buscar'),
                     ),
