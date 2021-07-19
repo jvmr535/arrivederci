6e9233babc4c22ea4a2805ccc13ca6adefe06b94
Alan Pinho <alanqupinho@gmail.com>
2021-07-19 20:49:28 -0300

    feat: :sparkles: Travel Itinerary Detail Page and Model

--- lib/modules/travel_itinerary_detail/travel_itinerary_detail_page.dart
+++ lib/modules/travel_itinerary_detail/travel_itinerary_detail_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:arrivederci/shared/models/travel_itinerary_detail_model.dart';
 import 'package:arrivederci/shared/themes/app_colors.dart';
 import 'package:arrivederci/shared/themes/app_footer.dart';
@@ -5,10 +7,9 @@ import 'package:arrivederci/shared/themes/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class TravelDetailPage extends StatelessWidget {
-  //final TravelItinerary edit;
+  final TravelItinerary edit;
   
-  const TravelDetailPage({ Key? key }) : super(key: key);
-
+  const TravelDetailPage({ Key? key, required this.edit }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,33 +25,66 @@ class TravelDetailPage extends StatelessWidget {
             height: size.height * 0.7,
             child: Column(
               children: [
-                SizedBox(
-                  height: size.height * 0.05,
-                ),
                 ListTile(
                   title: Text(
-                    "this.place.name",
+                    //this.place.name,
+                    "Torre de Eiffel",
+                    textAlign: TextAlign.center,
                     style: TextStyles.textBlack,
                   ),
                 ),
-                ListTile(
-                  subtitle: Text("this.place.address"),
-                ),
                 Image.network(
-                  '/assets/images/logo.png',
+                  //'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.place.photo}&key=$apiKey',
+                  'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/74/aa/fc.jpg',
                   width: size.width,
-                  height: size.height * 0.4,
+                  height: size.height * 0.25,
+                  fit: BoxFit.fitWidth,
                 ),
-                Text("Avaliação: \$ {this.place.rating}"),
-                TextButton(
-                  onPressed: () {
-                    print("teste");
-                  },
-                  child: Text(
-                    "+ ADICIONAR AO ROTEIRO",
-                    style: TextStyle(color: AppColors.primary),
-                  ),
-                )
+                Row(
+                  children: [
+                    Container(
+                      
+                      
+                    )
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 8.0, top: 8.0),
+                      child: Text(
+                        //"Avaliação: ${this.place.rating}"
+                        "Avaliação: 5.0",
+                        style: TextStyles.textDetails,
+                      )
+                    ),
+                  ],
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        print("CONFIRMAR_teste");
+                      },
+                      child: Text(
+                        "CONFIRMAR",
+                        style: TextStyle(color: AppColors.primary),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        print("EXCLUIR_teste");
+                      },
+                      child: Text(
+                        "EXCLUIR",
+                        style: TextStyle(color: AppColors.primary),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
--- lib/shared/models/travel_itinerary_detail_model.dart
+++ lib/shared/models/travel_itinerary_detail_model.dart
@@ -3,15 +3,17 @@ import 'package:arrivederci/shared/models/place_model.dart';
 class TravelItinerary {
   String name;
   String photo;
+  String details;
   List <Place> places ;
 
   TravelItinerary(
       {required this.name,
       required this.photo,
+      required this.details,
       required this.places});
 
   @override
   String toString() {
-    return 'Place(Nome : $name, places: $places, referencia de imagem: $photo)';
+    return 'Place(Nome : $name, places: $places, details $details, referencia de imagem: $photo)';
   }
 }
