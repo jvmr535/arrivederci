b69dc512577b8b161688af247b447ede5abe1b29
Alan Pinho <alanqupinho@gmail.com>
2021-07-19 21:58:12 -0300

    refactor: :recycle: Rework About Page

--- lib/modules/about/about_page.dart
+++ lib/modules/about/about_page.dart
@@ -1,6 +1,6 @@
+import 'package:arrivederci/shared/themes/app_colors.dart';
 import 'package:arrivederci/shared/themes/app_footer.dart';
 import 'package:arrivederci/shared/themes/app_text_styles.dart';
-import 'package:arrivederci/shared/widgets/navigation_drawer/navigation_drawer_widget.dart';
 import 'package:flutter/material.dart';
 
 class About extends StatelessWidget {
@@ -8,38 +8,60 @@ class About extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text("Sobre"),
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
-            child:Text(
-            "Trabalho da disciplina de Programação para Dispositivos Móveis",
-            style: TextStyles.textAbout,
-            textAlign: TextAlign.center,
+      body: Card(
+          child: Container(
+            height: size.height * 0.32,
+            margin: EdgeInsets.only(top:10.0),
+            child: Column(children: [
+              ListTile(
+                
+                title: Text(
+                  //this.place.name,
+                  "Trabalho da disciplina de Programação para Dispositivos Móveis",
+                  textAlign: TextAlign.center,
+                  style: TextStyles.textBlack,
+                ),
+              ),
+              Divider(
+                color: AppColors.primary,
+                indent: 20,
+                endIndent: 20,
+              ),
+              Row(
+                children: [
+                  Container(
+                  margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
+                  child:
+                    Text(
+                      "Alunos:",
+                      style: TextStyles.textAbout,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                  margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
+                  child:
+                    Text(
+                      "Alan Queiroz Pinho \n"+
+                      "Ângelo T. Bittencourt Todeschini\n"+
+                      "João Vitor de Moura Rosa",
+                      style: TextStyles.textAbout,
+                    ),
+                  ),
+                ],
+              ),
+            ],
             ),
           ),
-          Container(
-            margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
-            child:Text(
-            "Alunos:",
-            style: TextStyles.textAbout,
-            ),
           ),
-          Container(
-            margin: EdgeInsets.only(top:20.0, left: 20.0, right: 20.0),
-            child:Text(
-            "Alan Queiroz Pinho \n"+
-            "Ângelo T. Bittencourt Todeschini\n"+
-            "João Vitor de Moura Rosa",
-            style: TextStyles.textAbout,
-            ),
-          )
-        ],
-      ),
       bottomNavigationBar: AppFooter.footer,
     );
   }
