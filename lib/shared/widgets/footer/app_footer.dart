6186fc4f02d3e1ca6d238d90f4e847d44a22c259
Alan Pinho <alanqupinho@gmail.com>
2021-08-14 11:45:34 -0300

    refactor: :recycle: Move footer to another folder

--- lib/shared/widgets/footer/app_footer.dart
+++ lib/shared/widgets/footer/app_footer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:arrivederci/shared/Constants.dart';
-import 'app_colors.dart';
-import 'app_text_styles.dart';
+import '../../themes/app_colors.dart';
+import '../../themes/app_text_styles.dart';
 
 class AppFooter extends StatelessWidget {
   const AppFooter({Key? key}) : super(key: key);
