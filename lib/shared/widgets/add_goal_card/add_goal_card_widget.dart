774c3f4ca82ee133870e3ad64cf0d62298f1e83a
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-08-14 14:21:55 -0300

    refactor: :recycle: Location screen

--- lib/shared/widgets/add_goal_card/add_goal_card_widget.dart
+++ lib/shared/widgets/add_goal_card/add_goal_card_widget.dart
@@ -23,36 +23,38 @@ class AddGoalCard extends StatelessWidget {
           child: Container(
             width: size.width * 0.9,
             height: size.height * 0.7,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: size.height * 0.05,
-                ),
-                ListTile(
-                  title: Text(
-                    this.place.name,
-                    style: TextStyles.textBlack,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: size.height * 0.05,
                   ),
-                ),
-                ListTile(
-                  subtitle: Text(this.place.address),
-                ),
-                Image.network(
-                  'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.place.photo}&key=$apiKey',
-                  width: size.width,
-                  height: size.height * 0.4,
-                ),
-                Text("Avaliação: ${this.place.rating}"),
-                TextButton(
-                  onPressed: () {
-                    print("teste");
-                  },
-                  child: Text(
-                    "+ ADICIONAR AO ROTEIRO",
-                    style: TextStyle(color: AppColors.primary),
+                  ListTile(
+                    title: Text(
+                      this.place.name,
+                      style: TextStyles.textBlack,
+                    ),
                   ),
-                )
-              ],
+                  ListTile(
+                    subtitle: Text(this.place.address),
+                  ),
+                  Image.network(
+                    'https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${this.place.photo}&key=$apiKey',
+                    width: size.width,
+                    height: size.height * 0.4,
+                  ),
+                  Text("Avaliação: ${this.place.rating}"),
+                  TextButton(
+                    onPressed: () {
+                      print("teste");
+                    },
+                    child: Text(
+                      "+ ADICIONAR AO ROTEIRO",
+                      style: TextStyle(color: AppColors.primary),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
