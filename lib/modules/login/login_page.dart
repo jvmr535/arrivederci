29553daf43d29a7fac05804c94002ce1ab56de12
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-08-14 12:25:16 -0300

    feat: :sparkles: Login form validator

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -13,6 +13,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   String _email = "", _password = "";
   final auth = FirebaseAuth.instance;
+  final _formKey = GlobalKey<FormState>();
   @override
   void initState() {
     super.initState();
@@ -26,102 +27,130 @@ class _LoginState extends State<Login> {
       body: Stack(children: [
         Center(
           child: Container(
-              child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "LOGIN",
-                  style: TextStyles.pageTitle,
-                ),
-                SizedBox(height: 80.0),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        _email = value.trim();
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
+            child: SingleChildScrollView(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "LOGIN",
+                      style: TextStyles.pageTitle,
                     ),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    onChanged: (value) {
-                      setState(() {
-                        _password = value.trim();
-                      });
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Senha',
-                      border: OutlineInputBorder(),
+                    SizedBox(height: 80.0),
+                    Container(
+                      width: size.width * 0.8,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Por favor preencha o campo de e-mail";
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            _email = value.trim();
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                     ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(height: 30.0),
-                Container(
-                  width: size.width * 0.8,
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.primary, // background
-                      onPrimary: Colors.white, // foreground
+                    SizedBox(height: 20.0),
+                    Container(
+                      width: size.width * 0.8,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Por favor preencha o campo de senha";
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            _password = value.trim();
+                          });
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                      ),
                     ),
-                    onPressed: () async {
-                      try {
-                        await auth
-                            .signInWithEmailAndPassword(
-                                email: _email, password: _password)
-                            .then(
-                              (_) => {
-                                Navigator.of(context).pushNamed(HOME_SCREEN)
-                              },
+                    SizedBox(height: 30.0),
+                    Container(
+                      width: size.width * 0.8,
+                      height: 40,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: AppColors.primary, // background
+                          onPrimary: Colors.white, // foreground
+                        ),
+                        onPressed: () async {
+                          try {
+                            if (_formKey.currentState!.validate()) {
+                              await auth
+                                  .signInWithEmailAndPassword(
+                                      email: _email, password: _password)
+                                  .then((_) => {
+                                        Navigator.of(context)
+                                            .pushNamed(HOME_SCREEN)
+                                      });
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Campos vazios"),
+                                ),
+                              );
+                            }
+                          } catch (e) {
+                            print(e);
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Erro ao fazer login"),
+                              ),
                             );
-                      } catch (e) {
-                        print("Deu errado");
-                      }
-                    },
-                    child: Text('ENTRAR'),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Container(
-                  width: size.width * 0.8,
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.white,
-                      onPrimary: AppColors.primary,
-                      side: BorderSide(color: AppColors.primary),
+                          }
+                        },
+                        child: Text('ENTRAR'),
+                      ),
                     ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                      Navigator.of(context).pushNamed(REGISTER_SCREEN);
-                    },
-                    child: Text('CADASTRAR'),
-                  ),
+                    SizedBox(height: 10.0),
+                    Container(
+                      width: size.width * 0.8,
+                      height: 40,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: AppColors.white,
+                          onPrimary: AppColors.primary,
+                          side: BorderSide(color: AppColors.primary),
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                          Navigator.of(context).pushNamed(REGISTER_SCREEN);
+                        },
+                        child: Text('CADASTRAR'),
+                      ),
+                    ),
+                    SizedBox(height: 10.0),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        primary: AppColors.primary,
+                      ),
+                      onPressed: () {
+                        Navigator.pop(context);
+                        Navigator.of(context).pushNamed(FORGOT_PASSWORD_SCREEN);
+                      },
+                      child: Text("Esqueceu a senha?"),
+                    )
+                  ],
                 ),
-                SizedBox(height: 10.0),
-                TextButton(
-                  style: TextButton.styleFrom(
-                    primary: AppColors.primary,
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                    Navigator.of(context).pushNamed(FORGOT_PASSWORD_SCREEN);
-                  },
-                  child: Text("Esqueceu a senha?"),
-                )
-              ],
+              ),
             ),
-          )),
+          ),
         )
       ]),
     );
