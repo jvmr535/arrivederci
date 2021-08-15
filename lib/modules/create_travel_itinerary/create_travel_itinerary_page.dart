221bf9aa1be91f4cdbb416f90dab244aec5be794
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-08-15 10:24:24 -0300

    feat: :sparkles: Create travel itineraries

--- lib/modules/create_travel_itinerary/create_travel_itinerary_page.dart
+++ lib/modules/create_travel_itinerary/create_travel_itinerary_page.dart
@@ -1,16 +1,18 @@
 import 'package:arrivederci/shared/themes/app_colors.dart';
 import 'package:arrivederci/shared/widgets/app_footer/app_footer_widget.dart';
 import 'package:arrivederci/shared/themes/app_text_styles.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class CreateTravelItinerary extends StatefulWidget {
-  const CreateTravelItinerary({Key? key}) : super(key: key);
-
   @override
   _CreateTravelItineraryState createState() => _CreateTravelItineraryState();
 }
 
 class _CreateTravelItineraryState extends State<CreateTravelItinerary> {
+  final auth = FirebaseAuth.instance;
+  String _travelItinerary = "";
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -22,37 +24,64 @@ class _CreateTravelItineraryState extends State<CreateTravelItinerary> {
       body: Stack(children: [
         Center(
           child: Container(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Criar roteiro",
-                  style: TextStyles.pageTitle,
-                ),
-                SizedBox(height: 80.0),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Nome do roteiro',
-                      border: OutlineInputBorder(),
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Criar roteiro",
+                    style: TextStyles.pageTitle,
+                  ),
+                  SizedBox(height: 80.0),
+                  Container(
+                    width: size.width * 0.8,
+                    child: TextFormField(
+                      onChanged: (value) {
+                        setState(() {
+                          _travelItinerary = value.trim();
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Nome do roteiro',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(height: 10.0),
-                Container(
-                  width: size.width * 0.8,
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.primary, // background
-                      onPrimary: Colors.white, // foreground
+                  SizedBox(height: 10.0),
+                  Container(
+                    width: size.width * 0.8,
+                    height: 40,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: AppColors.primary, // background
+                        onPrimary: Colors.white, // foreground
+                      ),
+                      onPressed: () async {
+                        try {
+                          await FirebaseDatabase.instance
+                              .reference()
+                              .child(
+                                  "users/${auth.currentUser!.uid}/itineraries/")
+                              .push()
+                              .set({"name": _travelItinerary});
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Roteiro criado com sucesso!"),
+                            ),
+                          );
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text("Erro ao criar roteiro"),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text('Criar'),
                     ),
-                    onPressed: () {},
-                    child: Text('Criar'),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         )
