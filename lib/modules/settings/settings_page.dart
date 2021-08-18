64d67faa967fc5e5d07c1e263ee7726ed443bab5
Alan Pinho <alanqupinho@gmail.com>
2021-08-17 22:12:19 -0300

    feat: :sparkles: Change name now works

--- lib/modules/settings/settings_page.dart
+++ lib/modules/settings/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:arrivederci/shared/themes/app_colors.dart';
 import 'package:arrivederci/shared/widgets/app_footer/app_footer_widget.dart';
 import 'package:arrivederci/shared/themes/app_text_styles.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatefulWidget {
@@ -11,6 +12,51 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final auth = FirebaseAuth.instance;
+  final _formNameKey = GlobalKey<FormState>();
+  final _formSenhalKey = GlobalKey<FormState>();
+  String _name = "", _password = "", _confirmPassword = "";
+
+  Future setName(String userName, GlobalKey<FormState> nameKey) async {
+    try {
+      if (nameKey.currentState!.validate()) {
+        await auth.currentUser!.updateDisplayName(userName);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Nome Alterado com sucesso!"),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Erro ao alterar o nome!"),
+        ),
+      );
+    }
+  }
+
+  Future setPassword(String email, GlobalKey<FormState> emailKey) async {
+    try {
+      if (emailKey.currentState!.validate()) {
+        await auth.currentUser!.updateEmail(email);
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("E-mail Alterado com sucesso!"),
+          ),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Erro ao alterar o e-mail!"),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -21,13 +67,12 @@ class _SettingsState extends State<Settings> {
       ),
       backgroundColor: AppColors.white,
       body: Stack(children: [
-        Center(
-          child: Container(
-              child: SingleChildScrollView(
+        Container(
+          margin: EdgeInsets.only(top: 20),
+          child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 10),
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Container(
@@ -45,12 +90,26 @@ class _SettingsState extends State<Settings> {
                   indent: 20,
                   endIndent: 20,
                 ),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Novo Nome',
-                      border: OutlineInputBorder(),
+                Form(
+                  key: _formNameKey,
+                  child: Container(
+                    width: size.width * 0.8,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Por favor preencha o campo de nome";
+                        }
+                        return null;
+                      },
+                      onChanged: (value) async {
+                        setState(() {
+                          _name = value.trim();
+                        });
+                      },
+                      decoration: InputDecoration(
+                        labelText: 'Novo Nome',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
                 ),
@@ -63,105 +122,16 @@ class _SettingsState extends State<Settings> {
                       primary: AppColors.primary, // background
                       onPrimary: Colors.white, // foreground
                     ),
-                    onPressed: () {},
+                    onPressed: () async {
+                      await setName(_name, _formNameKey);
+                    },
                     child: Text('ALTERAR'),
                   ),
                 ),
                 SizedBox(height: 20.0),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    width: 140,
-                    child: Text(
-                      "Alterar Email",
-                      style: TextStyles.textBlack,
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ),
-                const Divider(
-                  height: 20,
-                  thickness: 1,
-                  indent: 20,
-                  endIndent: 20,
-                ),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Novo Email',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Container(
-                  width: size.width * 0.8,
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.primary, // background
-                      onPrimary: Colors.white, // foreground
-                    ),
-                    onPressed: () {},
-                    child: Text('ALTERAR'),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Container(
-                    width: 140,
-                    child: Text(
-                      "Alterar Senha",
-                      style: TextStyles.textBlack,
-                      textAlign: TextAlign.end,
-                    ),
-                  ),
-                ),
-                const Divider(
-                  height: 20,
-                  thickness: 1,
-                  indent: 20,
-                  endIndent: 20,
-                ),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Nova Senha',
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                Container(
-                  width: size.width * 0.8,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Confirmar senha',
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                SizedBox(height: 30.0),
-                Container(
-                  width: size.width * 0.8,
-                  height: 40,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: AppColors.primary, // background
-                      onPrimary: Colors.white, // foreground
-                    ),
-                    onPressed: () {},
-                    child: Text('ALTERAR'),
-                  ),
-                ),
               ],
             ),
-          )),
+          ),
         )
       ]),
       bottomNavigationBar: AppFooter(),
